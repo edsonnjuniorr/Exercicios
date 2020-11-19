b91724d55f990d71aa7f11dad2a82eae0c87bdc0
Edson Jr <edson.natel@catolicasc.org.br>
2020-11-18 21:21:50 -0300

    Exercícios 18/11/2020

---
+++ src/PersonTest.java
@@ -0,0 +1,17 @@
+public class PersonTest {
+    public static void main(String[] args) {
+        Person p = new Person("José", "R. Bolinha, 123");
+        System.out.println(p);
+        p.setAddress("R. Quadrado, 312");
+        System.out.println(p);
+
+        Staff s = new Staff("Pedro", "R. 9, 300", "Católica SC", 1000);
+        System.out.println(s);
+        System.out.println("Nome: " + s.getName());
+        s.setAddress("R. Cascal, 3");
+        System.out.println(s);
+
+        Student st = new Student("João", "João da Silva", "Teste", 2, 800);
+    }
+}
+
